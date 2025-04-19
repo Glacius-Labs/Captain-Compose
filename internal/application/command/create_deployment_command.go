2b897943c11da4f4a39633ed4546a9feefbd5de5
TheGlacius <core.glacius@gmail.com>
2025-04-19 23:59:27 +0200

    Add Deployment field to CreateDeploymentCommand struct

--- internal/application/command/create_deployment_command.go
+++ internal/application/command/create_deployment_command.go
@@ -1,6 +1,9 @@
 package command
 
+import "github.com/glacius-labs/captain-compose/internal/core/model"
+
 type CreateDeploymentCommand struct {
+	Deployment model.Deployment
 }
 
 func (CreateDeploymentCommand) IsCommand() {}
